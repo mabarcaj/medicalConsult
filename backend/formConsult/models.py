c66c1cd877e2e7368b23a0b1d587ab62823b92ef
mabarcaj <mabarcaj1@gmail.com>
2021-12-04 23:40:30 -0300

    Added Consulta model and Doctor model

--- backend/formConsult/models.py
+++ backend/formConsult/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
+from .validators import validate_age, validate_rut
 
-# Create your models here.
+
+class SexClass(models.TextChoices):
+    MALE = 'M', 'Masculino'
+    FEMALE = 'F', 'Femenino'
+    OTHER = 'X', 'Otro'
+
+
+class Doctor(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=60)
+    especialidad = models.CharField(max_length=70)
+    # horarios = ArrayField(models.DateTimeField()) # convert later
+    horario_disponible = models.DateTimeField('horario disponible')
+
+
+class Consulta(models.Model):
+    # Nombres
+    primer_nombre = models.CharField(max_length=50, default='A')
+    segundo_nombre = models.CharField(max_length=50, default='B')
+
+    # Apellido Paterno
+    apellido_paterno = models.CharField(max_length=50, default='C')
+
+    # Apellido Materno
+    apellido_materno = models.CharField(max_length=50, default='D')
+
+    # Rut
+    rut = models.CharField(max_length=12, validators=[validate_rut], default="12345678-9")
+
+    # Edad
+    edad = models.IntegerField(validators=[validate_age])
+
+    # Sexo
+    sexo = models.CharField(max_length=1, choices=SexClass.choices)
+
+    # Nombre Medico
+    # nombre_medico = models.ForeignKey(Doctor, on_delete=models.CASCADE, default= 1)
+    nombr_medico = models.CharField(max_length=50, default='John Doe')
+
+    # Fecha y Hora Consulta
+    fecha_consulta = models.DateTimeField('fecha consulta') # maybe this will be a class later on
+
+    # Correo
+    email = models.EmailField(max_length=200)  # restrict to specefic domains?
+
+    def __str__(self):
+        return '{} {}'.format(self.primer_nombre, self.apellido_paterno)
---
+++ backend/formConsult/validators.py
@@ -0,0 +1,70 @@
+from django.core.exceptions import ValidationError
+
+# Const and Hyperparameters
+AGE_ERROR_MSG = 'Edad inválida'
+RUT_ERROR_MSG = 'Rut Inválido'
+
+
+def isAdult(age):
+    return age >= 18
+
+
+# Age Validaton Method
+def validate_age(value):
+    if not isAdult(value):
+        raise ValidationError(AGE_ERROR_MSG)
+    return value
+
+
+def getParts(rut):
+    serie = rut[:-1]
+    verif = rut[-1]    
+
+    try:
+        verif = int(verif)
+    except:
+        verif = verif.upper()
+
+    return serie, verif
+
+
+def genRutIterables(serie):
+    rut = list()
+    for num in serie:
+        rut.append(int(num))
+    rut.reverse()
+
+    validSerie = [i for i in range(2, 8)]
+    delta = len(rut) - len(validSerie)
+
+    for i in range(delta):
+        validSerie.append(validSerie[i])
+
+    return rut, validSerie
+
+
+def isValid(value):
+    nonVerifDigits, verifDigit = getParts(value)
+    pseudoRut, validationSerial = genRutIterables(nonVerifDigits)
+    aux = list()
+
+    for i in range(len(pseudoRut)):
+        aux.append(validationSerial[i] * pseudoRut[i])
+
+    total = sum(aux)
+    trueVerif = 11 - (total % 11)
+
+    if trueVerif == 10:
+        trueVerif = 'K'
+
+    elif trueVerif == 11:
+        trueVerif = 0
+
+    return trueVerif == verifDigit
+
+
+# RUT Validation Method
+def validate_rut(value):
+    if not isValid(value):
+        raise ValidationError(RUT_ERROR_MSG)
+    return value
